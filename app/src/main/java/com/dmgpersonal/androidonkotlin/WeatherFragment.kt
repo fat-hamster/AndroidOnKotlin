869f447322211dc980f8b94f1abf7fea18a10b89
Dmitriy Gridchin <tigris.tstu@gmail.com>
2022-06-27 23:11:39 +0300

    Добавил sealed class. добавил AppState

--- app/src/main/java/com/dmgpersonal/androidonkotlin/WeatherFragment.kt
+++ app/src/main/java/com/dmgpersonal/androidonkotlin/WeatherFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dmgpersonal.androidonkotlin.databinding.FragmentWeatherBinding
+import com.google.android.material.snackbar.Snackbar
 
 class WeatherFragment : Fragment() {
 
@@ -17,13 +18,14 @@ class WeatherFragment : Fragment() {
     }
 
     private lateinit var viewModel : WeatherViewModel
-    private lateinit var binding : FragmentWeatherBinding
+    private var _binding : FragmentWeatherBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentWeatherBinding.inflate(layoutInflater)
+        _binding = FragmentWeatherBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -31,12 +33,34 @@ class WeatherFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
-        val observer = Observer<Any> { render(it) }
-        viewModel.getData().observe(viewLifecycleOwner, observer)
+        val observer = Observer<AppState> { renderData(it) }
+        viewModel.getLiveData().observe(viewLifecycleOwner, observer)
+        viewModel.getWeather()
     }
 
-    private fun render(data: Any) {
-        Toast.makeText(context, "data: $data", Toast.LENGTH_LONG).show()
+    private fun renderData(appState: AppState) {
+        when(appState) {
+            is AppState.Success -> {
+                val weatherData = appState.weatherData
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(requireView(), "Success", Snackbar.LENGTH_LONG).show()
+            }
+
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar.make(requireView(), "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeather() }
+                    .show()
+            }
+        }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/com/dmgpersonal/androidonkotlin/WeatherViewModel.kt
+++ app/src/main/java/com/dmgpersonal/androidonkotlin/WeatherViewModel.kt
@@ -5,16 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.lang.Thread.sleep
 
-class WeatherViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData()): ViewModel(){
-    fun getData(): LiveData<Any> {
-        getDataFromLocalSource()
-        return liveData
-    }
+class WeatherViewModel(private val liveData: MutableLiveData<AppState> = MutableLiveData()): ViewModel(){
+
+    fun getLiveData() = liveData
+    fun getWeather() = getDataFromLocalSource()
 
     private fun getDataFromLocalSource() {
         Thread {
+            liveData.postValue(AppState.Loading)
             sleep(2000)
-            liveData.postValue(Any())
+            liveData.postValue(AppState.Success(AppState.Success(Any())))
         }.start()
     }
 }
