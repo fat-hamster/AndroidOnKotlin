3cded3cd1ba68194c8f574b9f54fce73c2309b56
Dmitriy Gridchin <tigris.tstu@gmail.com>
2022-07-12 01:46:48 +0300

    Добавил "отлов" ошибок в WeatherDTOModel

--- app/src/main/java/com/dmgpersonal/androidonkotlin/utils/Utils.kt
+++ app/src/main/java/com/dmgpersonal/androidonkotlin/utils/Utils.kt
@@ -6,6 +6,8 @@ import java.util.stream.Collectors
 class Utils {
 }
 
+const val YANDEX_API_KEY = "X-Yandex-API-Key"
+
 fun getLines(reader: BufferedReader): String {
     return reader.lines().collect(Collectors.joining("\n"))
 }
--- app/src/main/java/com/dmgpersonal/androidonkotlin/viewmodel/WeatherDTOModel.kt
+++ app/src/main/java/com/dmgpersonal/androidonkotlin/viewmodel/WeatherDTOModel.kt
@@ -3,26 +3,23 @@ package com.dmgpersonal.androidonkotlin.viewmodel
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dmgpersonal.androidonkotlin.BuildConfig
 import com.dmgpersonal.androidonkotlin.model.City
-import com.dmgpersonal.androidonkotlin.model.RemoteRepository
-import com.dmgpersonal.androidonkotlin.model.RemoteRepositoryImpl
 import com.dmgpersonal.androidonkotlin.model.dto.WeatherDTO
+import com.dmgpersonal.androidonkotlin.utils.YANDEX_API_KEY
 import com.dmgpersonal.androidonkotlin.utils.getLines
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.lang.Exception
 import java.net.MalformedURLException
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
-class WeatherDTOModel (
+class WeatherDTOModel(
     private val liveDataDTO: MutableLiveData<AppState> = MutableLiveData(),
-    private val repository: RemoteRepository =  RemoteRepositoryImpl()
 ) : ViewModel() {
 
     fun getLiveDataDTO() = liveDataDTO
@@ -33,38 +30,59 @@ class WeatherDTOModel (
     private fun getWeatherFromServer(city: City) {
         liveDataDTO.value = AppState.Loading
         requestToServer(city)
-
     }
 
     private fun requestToServer(city: City) {
-        val uri =
-            URL("https://api.weather.yandex.ru/v2/informers?lat=${city.lat}&lon=${city.lon}")
-        val connection: HttpsURLConnection?
-
+        var uri: URL? = null
         try {
-            connection = uri.openConnection() as HttpsURLConnection
-            connection.readTimeout = 10000
-            connection.addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
-
-            Thread {
-                try {
-                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-                    val weather = Gson().fromJson(getLines(reader), WeatherDTO::class.java)
-                    Handler(Looper.getMainLooper()).post {
-                        liveDataDTO.postValue(AppState.SuccessFromServer(weather))
-                    }
-                } catch (e : Exception) {
-                    liveDataDTO.postValue(AppState.Error(e))
-                    Log.e("@@@", "Cannot receive data from server", e)
-                    e.printStackTrace()
-                } finally {
-                    connection.disconnect()
-                }
-            }.start()
-        } catch (e : MalformedURLException) {
+            uri =
+                URL("https://api.weather.yandex.ru/v2/informers?lat=${city.lat}&lon=${city.lon}")
+        } catch (e: MalformedURLException) {
             liveDataDTO.value = AppState.Error(e)
             Log.e("@@@", "Fail URI", e)
             e.printStackTrace()
         }
+
+        var connection: HttpsURLConnection? = null
+
+        try {
+
+            connection = uri?.openConnection() as HttpsURLConnection
+            connection.also {
+                it.readTimeout = 5000
+                it.addRequestProperty(YANDEX_API_KEY, BuildConfig.WEATHER_API_KEY)
+            }
+
+            Thread {
+                val reader: BufferedReader
+                try {
+                    reader = BufferedReader(InputStreamReader(connection.inputStream))
+                } catch (e: RuntimeException) {
+                    liveDataDTO.postValue(AppState.Error(e))
+                    Log.e("@@@", "Cannot receive data from server", e)
+                    e.printStackTrace()
+                    return@Thread
+                }
+                val weather: WeatherDTO
+                try {
+                    weather = Gson().fromJson(getLines(reader), WeatherDTO::class.java)
+                } catch (e: JsonSyntaxException) {
+                    liveDataDTO.postValue(AppState.Error(e))
+                    Log.e("@@@", "Cannot receive data from server", e)
+                    e.printStackTrace()
+                    return@Thread
+                }
+                Handler(Looper.getMainLooper()).post {
+                    liveDataDTO.postValue(AppState.SuccessFromServer(weather))
+                }
+            }.start()
+        } catch (e: RuntimeException) {
+            liveDataDTO.postValue(AppState.Error(e))
+            Log.e("@@@", "Cannot receive data from server", e)
+            e.printStackTrace()
+        } finally {
+            connection?.disconnect()
+        }
+
     }
 }
