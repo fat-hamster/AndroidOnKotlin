95f733331d54052001e4d38f9c5dc0dc2aa58f34
Dmitriy Gridchin <tigris.tstu@gmail.com>
2022-07-13 23:32:33 +0300

    убрал слушателя из MainActivity, не нужен он, но опыт интересный

--- app/src/main/java/com/dmgpersonal/androidonkotlin/view/MainActivity.kt
+++ app/src/main/java/com/dmgpersonal/androidonkotlin/view/MainActivity.kt
@@ -2,10 +2,7 @@ package com.dmgpersonal.androidonkotlin.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.dmgpersonal.androidonkotlin.R
-import com.dmgpersonal.androidonkotlin.utils.ConnectionLiveData
-import com.dmgpersonal.androidonkotlin.utils.onAlertDialog
 import com.dmgpersonal.androidonkotlin.view.cities.CitiesListFragment
 
 class MainActivity : AppCompatActivity() {
@@ -20,16 +17,16 @@ class MainActivity : AppCompatActivity() {
                 .commitNow()
         }
 
-        val root = findViewById<ConstraintLayout>(R.id.rootLayout)
-        val connectionLiveData = ConnectionLiveData(root.context)
-        connectionLiveData.observe(this) { isConnected ->
-            isConnected?.let {
-                when (it) {
-                    true -> onAlertDialog(root, "Network is available")
-                    false -> onAlertDialog(root, "Network is not available")
-                }
-            }
-        }
+//        val root = findViewById<ConstraintLayout>(R.id.rootLayout)
+//        val connectionLiveData = ConnectionLiveData(root.context)
+//        connectionLiveData.observe(this) { isConnected ->
+//            isConnected?.let {
+//                when (it) {
+//                    true -> onAlertDialog(root, "Network is available")
+//                    false -> onAlertDialog(root, "Network is not available")
+//                }
+//            }
+//        }
     }
 
 }
