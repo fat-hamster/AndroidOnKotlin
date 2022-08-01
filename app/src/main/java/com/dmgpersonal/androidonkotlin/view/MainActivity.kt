d49f0a81114151f97a34946926cd44a93998bfc7
Dmitriy Gridchin <tigris.tstu@gmail.com>
2022-08-01 21:06:55 +0300

    правки по 10-му ДЗ

--- app/src/main/java/com/dmgpersonal/androidonkotlin/view/MainActivity.kt
+++ app/src/main/java/com/dmgpersonal/androidonkotlin/view/MainActivity.kt
@@ -24,8 +24,6 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.container, CitiesListFragment.newInstance())
                 .commitNow()
         }
-
-        showToken()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
